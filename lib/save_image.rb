283064c8c40d52aefe713500bab386f67162c850
sgourshete <swapnilggourshete@gmail.com>
2018-06-27 16:18:55 +0530

    1. First commit
    2. rest-client and nokogiri used
    
    3. Images are saved to project folder
    
    4. save_images method returns the number of images saved

---
+++ lib/save_image.rb
@@ -0,0 +1,35 @@
+require 'nokogiri'
+require 'rest-client'
+
+class SaveImage
+
+	def self.save_images(url)
+		successfully_saved_img = 0
+		
+		html   = RestClient.get("#{url}").body
+		parsed = Nokogiri::HTML.parse(html)
+ 
+		image_tags = parsed.css("img")
+		
+		image_tags.each do |img|
+		  if img['src'].split('http')[1].nil?
+				url  = "http:#{img['src']}"
+			else
+				url  = "#{img['src']}"	
+			end
+		  
+		  name = url.split("/").last
+		 
+		  begin
+		  	file = RestClient.get(url).body
+		  	successfully_saved_img += 1
+		  rescue Exception => e
+		  	
+		  end
+		 
+		  File.write(name, file, mode: "wb")
+		end
+		successfully_saved_img
+	end
+
+end
---
+++ save_image.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'save_image'
+  s.version     = '0.0.1'
+  s.date        = '2018-06-27'
+  s.summary     = "Save all images on a url"
+  s.description = "A simple gem to save all images from a given url."
+  s.authors     = ["Swapnil Gourshete"]
+  s.email       = 'swapnilggourshete@gmail.com'
+  s.files       = ["lib/save_image.rb"]
+  s.license     = ''
+end
